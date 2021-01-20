c32d8fd47666faf196840721ba77e9d913bd6d98
laurenausoleil <lauren.au.soleil@gmail.com>
2021-01-20 12:32:25 +0000

    add eruption length select

--- old_faithful_app/app.R
+++ old_faithful_app/app.R
@@ -3,39 +3,59 @@ library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30),
+      # ----
+      # ADD THIS - double ended range selector
+      # ----
+      sliderInput(inputId = "eruptionLength",
+                  label = "Eruption length (secs):",
+                  min = 0,
+                  max = 10,
+                  value = c(1, 5),
+                  step = 0.25),
+    ),
     
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-    
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput(inputId = "bins",
-                        label = "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput(outputId = "distPlot")
-        )
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput(outputId = "distPlot")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    
-    output$distPlot <- renderPlot({
-        # draw the histogram with the specified number of bins
-        faithful %>% ggplot(aes(x = waiting)) +
-            geom_histogram(bins = input$bins, col = "white", fill = "darkred") +
-            xlab("Waiting time (mins)") +
-            ylab("Number of eruptions") +
-            ggtitle("Histogram of eruption waiting times")
-    })
-    
+  # ----
+  # ADD THIS
+  # ----
+  data <- reactive(
+    faithful %>%
+      filter(
+        between(eruptions, input$eruptionLength[1], input$eruptionLength[2])
+      )
+  )
+  
+  output$distPlot <- renderPlot({
+    # draw the histogram with the specified number of bins
+    # ----
+    # CHANGE THIS TO data()
+    # ----
+    data() %>% ggplot(aes(x = waiting)) +
+      geom_histogram(bins = input$bins, col = "white", fill = "darkred") +
+      xlab("Waiting time (mins)") +
+      ylab("Number of eruptions") +
+      ggtitle("Histogram of eruption waiting times")
+  })
 }
 
 # Run the application 
